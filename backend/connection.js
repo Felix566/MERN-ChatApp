c47194928d20f2a3b982d72d89d17222b39fe326
Leandro Felix <leandro123felix123@gmail.com>
2023-04-13 13:50:10 -0300

    resolvendo problema de conexão com o mongoDB

--- backend/connection.js
+++ backend/connection.js
@@ -1,6 +1,14 @@
-const mongoose = require('mongoose');
 require('dotenv').config();
+const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.naz2bvi.mongodb.net/chatappMern?retryWrites=true&w=majority`, ()=>{
-    console.log('connected to mongoDB')
+main()
+.then(res=>{
+    console.log('conectado ao mongodb')
 })
+.catch(err => console.log(err));
+
+async function main(){
+    await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.naz2bvi.mongodb.net/chatappMern?retryWrites=true&w=majority`);
+}
+
+module.exports = mongoose;
