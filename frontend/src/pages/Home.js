01f0a482e95226ed2dd48d021d9a3dfe582390b8
Leandro Felix <leandro123felix123@gmail.com>
2023-04-13 09:46:49 -0300

    modificando pages

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Row, Col, Button } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import './Home.css';
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <Row>
+      <Col md={6} className='d-flex flex-direction-column align-itens-center justify-content-center'>
+        <div>
+          <h1>Compartilhe o mundo com seus amigos</h1>
+          <p>ChatApp permite que você se conecte com o mundo</p>
+          <LinkContainer to="/chat">
+            <Button variant='sucess'>
+              Vamos começar <i className='fas fa-comments home-message-icon'></i>
+            </Button>
+          </LinkContainer>
+        </div>
+      </Col>
+      <Col md={6} className='home_bg'></Col>
+    </Row>
+  );
 }
 
 export default Home
